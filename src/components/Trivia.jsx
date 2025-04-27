3961ab15dff6137a89f060b0c9c5f668772dd953
Abdulrezak <abodkhaled2003@gmail.com>
2025-04-27 22:25:14 +0300

    added trivia component

---
+++ src/components/Trivia.jsx
@@ -0,0 +1,13 @@
+export default function Trivia() {
+  return (
+    <div className="trivia">
+      <div className="question">What's the best youtube channel?</div>
+      <div className="answers">
+        <div className="answer">1</div>
+        <div className="answer">2</div>
+        <div className="answer">3</div>
+        <div className="answer">4</div>
+      </div>
+    </div>
+  );
+}
